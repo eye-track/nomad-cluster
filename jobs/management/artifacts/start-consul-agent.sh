166f780ab997c13e83664257bf0081096e4346e6
Enrico Buratto <enrico.buratto@light-speed.it>
2025-07-04 17:34:05 +0200

    update

--- jobs/management/artifacts/start-consul-agent.sh
+++ jobs/management/artifacts/start-consul-agent.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+echo "Unzip Consul agent..."
 unzip local/consul.zip consul -d local/
+
+echo "Starting Consul agent..."
 chmod +x local/consul
 local/consul agent -config=local/artifacts/cfg.consul.hcl
