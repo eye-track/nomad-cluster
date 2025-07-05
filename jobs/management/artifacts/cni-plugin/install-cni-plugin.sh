5363772c2502f722fab870b8da0194d0d01e289c
Enrico Buratto <enrico.buratto@light-speed.it>
2025-07-05 15:28:48 +0200

    update

--- jobs/management/artifacts/cni-plugin/install-cni-plugin.sh
+++ jobs/management/artifacts/cni-plugin/install-cni-plugin.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-echo "[START] Unzip CNI plugin to /etc/cni/bin..."
-mkdir -p /etc/cni/bin
-tar -C /etc/cni/bin -xzf local/cni-plugins.tgz
-echo "[END] Unzip CNI plugin to /etc/cni/bin..."
-
-while true; do
-  echo "Still waiting..."
-  sleep 60
-done
+echo "[START] Unzip CNI plugin to /opt/cni/bin..."
+mkdir -p /opt/cni/bin
+tar -C /opt/cni/bin -xzf local/cni-plugins.tgz
+echo "[END] Unzip CNI plugin to /opt/cni/bin..."
