23322edfb67ce59f6c014ca5e1944999d4a11348
Enrico Buratto <enrico.buratto@light-speed.it>
2025-07-05 15:09:22 +0200

    update

--- jobs/management/artifacts/cni-plugin/install-cni-plugin.sh
+++ jobs/management/artifacts/cni-plugin/install-cni-plugin.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo "Unzip CNI plugin to /opt/cni/bin..."
+echo "[START] Unzip CNI plugin to /opt/cni/bin..."
 sudo mkdir -p /opt/cni/bin
 sudo tar -C /opt/cni/bin -xzf local/cni-plugins.tgz
+echo "[END] Unzip CNI plugin to /opt/cni/bin..."
--- jobs/management/artifacts/consul-agent/start-consul-agent.sh
+++ jobs/management/artifacts/consul-agent/start-consul-agent.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "Unzip Consul agent..."
+echo "[START] Unzip Consul agent..."
 unzip -o local/consul.zip consul -d local/
+echo "[END] Unzip Consul agent"
 
-echo "Start Consul agent..."
+echo "[START] Consul agent..."
 chmod +x local/consul
 local/consul agent -config-file=local/artifacts/cfg.consul.hcl
+echo "[END] Consul agent"
