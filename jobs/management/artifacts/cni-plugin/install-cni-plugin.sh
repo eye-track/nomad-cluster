9a42c7a52c5421d4add2055b8d708a4f3064c634
Enrico Buratto <enrico.buratto@light-speed.it>
2025-07-05 15:14:19 +0200

    update

--- jobs/management/artifacts/cni-plugin/install-cni-plugin.sh
+++ jobs/management/artifacts/cni-plugin/install-cni-plugin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "[START] Unzip CNI plugin to /opt/cni/bin..."
-sudo mkdir -p /opt/cni/bin
-sudo tar -C /opt/cni/bin -xzf local/cni-plugins.tgz
+mkdir -p /opt/cni/bin
+tar -C /opt/cni/bin -xzf local/cni-plugins.tgz
 echo "[END] Unzip CNI plugin to /opt/cni/bin..."
 
 while true; do
